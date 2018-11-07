91c11d628f6a6959f0d47b17038837e71e36f646
Barry McCormick <barry.mccormick1961@gmail.com>
2018-11-07 00:21:05 -0700

    script and custom 404 page

--- install.sh
+++ install.sh
@@ -1,19 +1,49 @@
 #!/bin/sh
 
+MYDIR = "webbackend"
+MYPORT = "3200"
+
+# update system
+apt-get update
+apt-get -y upgrade
+
 # install nginx
 apt install nginx
 
-# enable at boot
+# confirm that nginx will boot at start
 systemctl enable nginx
 
-# make a copy of default conf for new site
-cp /etc/nginx/sites-available/default /etc/nginx/sites-available/vh-3200
+# Enable ufw firewall
+ufw enable 
 
-# replace port 80 with 3200
-sed -i -e 's/80/3200/g' /etc/nginx/sites-available/vh-3200
+# allow Nginx through firewall
+ufw allow 'Nginx HTTP'
+ufs status
+
+
+# make a copy of default conf for new site
+cp ./vh-3200 /etc/nginx/sites-available/vh-3200
+cp ./custom404.html /var/www/$MYDIR
+
+
+# replace #PORT# with 3200
+sed -i -e 's/#PORT#/'$MYPORT'/g' /etc/nginx/sites-available/vh-3200
+
+# replace #DIR# with webbackend
+sed -i -e 's/#DIR#/'$MYDIR'/g' /etc/nginx/sites-available/vh-3200
+
+
+sed -i -e 's/#SERVERNAME#/'$SERVERNAME'/g' /etc/nginx/sites-available/vh-3200
 
 # activate the new site
-ln -s /etc/nginx/sites-avialable/vh-3200 /etc/nginx/sites-enabled/002-vh-3200
+ln -s /etc/nginx/sites-available/vh-3200 /etc/nginx/sites-enabled/002-vh-3200
+
+
+
+
+
+
+
 
 
 
